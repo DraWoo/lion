1ed7c76ad56f04f3597ab07c84ce70bccfc488b3
dragonWoo <mire2566@naver.com>
2021-07-02 15:07:33 +0900

    logo images 적용 mire2566@gmail.com

--- src/main/java/com/god/lion/controller/BoardController.java
+++ src/main/java/com/god/lion/controller/BoardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/board")//localhost:8080/board 경로 지정
+@RequestMapping("/")//localhost:8080/board 경로 지정
 public class BoardController {
 
     @GetMapping("/list")
