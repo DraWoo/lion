dc36a1c2fd9f5b141246a18d638c2aa050ffe463
dragonWoo <mire2566@naver.com>
2021-07-02 15:09:20 +0900

    update controller mire2566@gmail.com

--- src/main/java/com/god/lion/controller/BoardController.java
+++ src/main/java/com/god/lion/controller/BoardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/")//localhost:8080/board 경로 지정
+@RequestMapping("/board")//localhost:8080/board 경로 지정
 public class BoardController {
 
     @GetMapping("/list")
