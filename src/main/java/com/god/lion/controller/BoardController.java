5806fd4bdf414005b11704a626ac6a634eda2522
dragonWoo <mire2566@naver.com>
2021-07-05 16:45:50 +0900

    list.html DB에서 -> th:each 활용 리스트 불러오기 완성 mire2566@gmail.com

--- src/main/java/com/god/lion/controller/BoardController.java
+++ src/main/java/com/god/lion/controller/BoardController.java
@@ -6,8 +6,7 @@ import com.god.lion.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,12 +18,35 @@ public class BoardController {
     private BoardRepository boardRepository;
 
 
+    //게시판 조회
     @GetMapping("/list")
     public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
-        model.addAttribute("boards", boards);
+        List<Board> boards = boardRepository.findAll(); //jpa api-> find all(메소드) ->전체조회
+        model.addAttribute("boards", boards);//글 저장을 하려면 model에 key 값이 있어야 한다
 
         return "board/list";
     }
+    /*form 태크 -> 글 쓰기 구현*/
+    @GetMapping("/form")
+    //필요한 경우에만 아이디 값을 사용해서 수정하겠다 required = false
+    public  String form(Model model, @RequestParam(required = false) Long id){
+        if (id == null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);//findById로 조회한 아이디가 없을수도 있기때문에 이 경우에는 .orElse로 (null)주면 된다.
+            model.addAttribute("board", board);
+        }
+
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+//  PostMapping 으로 form 태그를 받아옴
+    //글 저장
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Board board){
+        boardRepository.save(board);//jpa api-> save(메소드) ->저장 board로 전달
+        return "redirect:/board/list";// return "redirect:board/list"; 해주면 다시 조회하면으로 이동해서 조회를 하게 해준다
+    }
+
 }
 
