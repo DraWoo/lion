c4f8ea89bda85149a78e49288dcbe4774f0cf503
dragonWoo <mire2566@naver.com>
2021-07-06 16:47:20 +0900

    페이징 추가 mire2566@gmail.com

--- src/main/java/com/god/lion/controller/BoardController.java
+++ src/main/java/com/god/lion/controller/BoardController.java
@@ -5,6 +5,9 @@ import com.god.lion.model.Board;
 import com.god.lion.repository.BoardRepository;
 import com.god.lion.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,10 +30,19 @@ public class BoardController {
 
     //게시판 조회
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll(); //jpa api-> find all(메소드) ->전체조회
+    //@PageableDefault(size = 5)->  한 페이지에 보여질 데이터 개수 표시
+    public String list(Model model, @PageableDefault(size = 5) Pageable pageable){
+        //PageRequest.of(1,20) -> jpa 페이징 처리 (returnType ->List<T>가 아니라 Page<T>로 해주면 된다.
+        Page<Board> boards = boardRepository.findAll(pageable); //jpa api-> find all(메소드) ->전체조회
+        //페이징 처리
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() -4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() +4);
+        /*boards.getPageable().getPageNumber() 현재 페이지를 알려주는 구문*/
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        //전체개수를 총 게시글 확인
+        /*boards.getTotalElements();view에 적용전 디버깅해서 미리 확인 후 적용*/
         model.addAttribute("boards", boards);//글 저장을 하려면 model에 key 값이 있어야 한다
-
         return "board/list";
     }
     /*form 태크 -> 글 쓰기 구현*/
@@ -40,7 +52,8 @@ public class BoardController {
         if (id == null){
             model.addAttribute("board", new Board());
         }else{
-            Board board = boardRepository.findById(id).orElse(null);//findById로 조회한 아이디가 없을수도 있기때문에 이 경우에는 .orElse로 (null)주면 된다.
+            //findById로 조회한 아이디가 없을수도 있기때문에 이 경우에는 .orElse로 (null)주면 된다.
+            Board board = boardRepository.findById(id).orElse(null);
             model.addAttribute("board", board);
         }
         return "board/form";
@@ -48,13 +61,16 @@ public class BoardController {
 //  PostMapping 으로 form 태그를 받아옴
     //글 저장
     @PostMapping("/form")
-    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){ //model 클래스에서 지정한 사이즈가 min2 -> max30 에 부합되지 않는다면 true,false 결과 도출
+    //model 클래스에서 지정한 사이즈가 min2 -> max30 에 부합되지 않는다면 true,false 결과 도출
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
         boardValidator.validate(board, bindingResult);
         if (bindingResult.hasErrors()){
             return "board/form";
         }
-        boardRepository.save(board);//jpa api-> save(메소드) ->저장 board로 전달
-        return "redirect:/board/list";// return "redirect:board/list"; 해주면 다시 조회하면으로 이동해서 조회를 하게 해준다
+        //jpa api-> save(메소드) ->저장 board로 전달
+        boardRepository.save(board);
+        ;// return "redirect:board/list"; 해주면 다시 조회하면으로 이동해서 조회를 하게 해준다
+        return "redirect:/board/list";
     }
 
 }
