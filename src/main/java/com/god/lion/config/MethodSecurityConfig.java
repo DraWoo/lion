8eacc0ca664435325f541666a14bab0ba19d142f
dragonWoo <mire2566@naver.com>
2021-07-13 13:17:40 +0900

    로그인 게시판 쓰기,수정,삭제,검색 완료
    mire2566@gmail.com

---
+++ src/main/java/com/god/lion/config/MethodSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.god.lion.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+//삭제시 보안 취약성을 보완하기 위한 설정 클래스
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+
+}
